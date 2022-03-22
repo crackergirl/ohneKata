3930b56db326dfe595df5e8ec5a11c377fa6f089
crackergirl <oihanaerroalzueta@gmail.com>
2022-03-22 08:10:33 +0000

    [] -Test:grumphp5

--- tests/OhceTest.php
+++ tests/OhceTest.php
@@ -42,7 +42,7 @@ final class OhceTest extends TestCase
 
         $ohceResponse = $ohce->execute("oto");
 
-        $this->assertEquals("oto,Bonita palabraa", $ohceResponse);
+        $this->assertEquals("oto,Bonita palabra", $ohceResponse);
     }
     /*
     $timeManager = Mockery::mock(TimeManager::class)
